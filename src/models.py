a586f530b45a9a77d3da2dc33658f81a47c8e3df
Andrey Tumerkin <tumerrkin@gmail.com>
2024-07-26 12:55:53 +0300

    add additional work

--- src/models.py
+++ src/models.py
@@ -1,12 +1,31 @@
 class Product:
-    """Класс для прдставления продуктов"""
+    """Класс для предоставления продуктов"""
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         """Метод для инициализации экземпляра класса Product, задаем значения атрибутам экземпляра"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, name: str, description: str, price: float, quantity: int):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """Геттер получает и выводит цену продукта"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int):
+        """Сеттер изменяет цену продукта, если ниже требует потверждения"""
+        if new_price < self.__price:
+            confirmation_price = input(f"Новая цена: {new_price} ниже {self.__price}, хотите изменить?(YES/NO)").lower()
+            if "y" in confirmation_price:
+                self.__price = new_price
+            else:
+                pass
 
 
 class Category:
@@ -19,6 +38,21 @@ class Category:
         """Метод для инициализации класса Category, задаем значения атрибутам экземпляра"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def views_product(self):
+        """Метод возвращает описание товара типа: Имя, цена, остаток."""
+        views_str = ""
+        for i in self.__products:
+            views_str += f"{i.name}, {i.price}. Остаток: {i.quantity}\n"
+
+        return views_str
+
+    @views_product.setter
+    def views_product(self, product: Product):
+        """Сеттер добавляет новый продукт в список продуктов категории"""
+        self.__products.append(product)
+        Category.product_count += 1
